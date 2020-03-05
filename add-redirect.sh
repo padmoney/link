c3e3951f7ab2d899c2740106262d51eb35b532d0
Breno Martinusso <martinusso@gmail.com>
2020-03-05 09:23:57 -0300

    add link emitindo boleto em sandbox com php e curl

--- add-redirect.sh
+++ add-redirect.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-mkdir $1
 echo "<script>location=\"$2\"</script>
-<noscript><a href=\"$2\">Link externo</a></noscript>" >> $1/index.html
+<noscript><a href=\"$2\">Link externo</a></noscript>" >> $1.html
